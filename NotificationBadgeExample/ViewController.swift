ea94780a576aa104a1282bcb099431a13569d452
Christoph Fuchs <christoph@mohemian.com>
2017-10-24 10:09:57 +0200

    Code formatting

--- NotificationBadgeExample/ViewController.swift
+++ NotificationBadgeExample/ViewController.swift
@@ -18,31 +18,30 @@ class ViewController: UIViewController {
     @IBOutlet var fontPreviewLabel: UILabel!
     @IBOutlet var fontSizeStepper: UIStepper!
     @IBOutlet var stepper: UIStepper!
-    
+
     @IBOutlet weak var barButton: UIBarButtonItem!
-    var currentFontSize:CGFloat = 12
-    
+    var currentFontSize: CGFloat = 12
+
     lazy var rightItem: UIBarButtonItem = UIBarButtonItem(title: "Test", style: .plain, target: nil, action: nil)
-    
+
     @IBAction func fontStepper(_ sender: UIStepper) {
-        
+
         fontPreviewLabel.font = UIFont.systemFont(ofSize: CGFloat(sender.value))
         currentFontSize = CGFloat(sender.value)
     }
-    
+
     var counter = 0
-    
+
     @IBAction func menuClick(_ sender: UIBarButtonItem) {
         counter += 1
         sender.badge(text: "\(counter)")
     }
-    
-    
+
     @IBAction func update(_ sender: AnyObject) {
-        var text:String?
+        var text: String?
         if badgeTextChanger.text?.characters.count == 0 {
             text = nil
-        }else{
+        } else {
             text = badgeTextChanger.text
         }
         var appearance = BadgeAppearance()
@@ -52,26 +51,23 @@ class ViewController: UIViewController {
         //appearance.textSize = CGFloat(stepper.value)
         appearance.allowShadow = true
         appearance.backgroundColor = UIColor(colorLiteralRed: redSlider.value, green: greenSlider.value, blue: blueSlider.value, alpha: 1)
-        
+
         badgeLabel.badge(text: text, appearance: appearance)
-        
-        
-        
+
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         stepper.maximumValue = 30
         stepper.minimumValue = 5
         //badgeLabel.badge(text: "test")
         //self.rightItem.badge(text: "lol")
         self.navigationItem.rightBarButtonItem = self.rightItem
         // Do any additional setup after loading the view, typically from a nib.
-        
-        
+
     }
-    
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         //badgeLabel.badge(text: "2")
@@ -82,10 +78,8 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        
+
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
